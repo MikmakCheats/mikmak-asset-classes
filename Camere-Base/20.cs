db27f9347c7123679c54d822983b0c2e0648a72f
louiml <99025570+Louiml@users.noreply.github.com>
2024-01-31 23:50:46 +0200

    Create 20.cs

---
+++ Camere-Base/20.cs
@@ -0,0 +1,46 @@
+Camera Base 139 False
+	PPtr<GameObject> m_GameObject 12 False
+		int m_FileID 4 False
+		SInt64 m_PathID 8 False
+	UInt8 m_Enabled 1 True
+	unsigned int m_ClearFlags 4 False
+	ColorRGBA m_BackGroundColor 16 False
+		float r 4 False
+		float g 4 False
+		float b 4 False
+		float a 4 False
+	int m_projectionMatrixMode 4 False
+	int m_GateFitMode 4 True
+	Vector2f m_SensorSize 8 False
+		float x 4 False
+		float y 4 False
+	Vector2f m_LensShift 8 False
+		float x 4 False
+		float y 4 False
+	float m_FocalLength 4 False
+	Rectf m_NormalizedViewPortRect 16 False
+		float x 4 False
+		float y 4 False
+		float width 4 False
+		float height 4 False
+	float near clip plane 4 False
+	float far clip plane 4 False
+	float field of view 4 False
+	bool orthographic 1 True
+	float orthographic size 4 False
+	float m_Depth 4 False
+	BitField m_CullingMask 4 False
+		unsigned int m_Bits 4 False
+	int m_RenderingPath 4 False
+	PPtr<RenderTexture> m_TargetTexture 12 False
+		int m_FileID 4 False
+		SInt64 m_PathID 8 False
+	int m_TargetDisplay 4 False
+	int m_TargetEye 4 False
+	bool m_HDR 1 False
+	bool m_AllowMSAA 1 False
+	bool m_AllowDynamicResolution 1 False
+	bool m_ForceIntoRT 1 False
+	bool m_OcclusionCulling 1 True
+	float m_StereoConvergence 4 False
+	float m_StereoSeparation 4 False
